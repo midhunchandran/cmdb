81026914abf998eb22d5f5847fd6b10e60402fe3
Midhun Chandran <midhunchandran@gmail.com>
2017-04-23 07:45:33 +0000

    Reading instance details from config file

--- servicenow.py
+++ servicenow.py
@@ -11,16 +11,20 @@ import logging
 
 
 class ServiceNowAPI:
-    def __init__(self):
-        self.base_uri = 'https://dev17199.service-now.com/'
-        self.user = 'apiuser'
-        self.password = '9a9j0D%V'
+    #def __init__(self):
+    #    self.base_uri = 'https://instance.service-now.com/'
+
+    def set_base_uri(self, base_uri):
+        self.base_uri = base_uri
+
+    def set_auth(self, username, password):
+        self.auth = HTTPBasicAuth(username, password)
 
     def get_table_schema(self, table_name):
         uri = self.base_uri + 'api/now/v1/table/sys_dictionary?name=' + table_name
         print uri
         headers = {'accept' : 'application/json'}
-        r = requests.get(uri, headers=headers, auth=HTTPBasicAuth(self.user, self.password))
+        r = requests.get(uri, headers=headers, auth=self.auth) #auth=HTTPBasicAuth(self.user, self.password))
 
         if r.status_code != requests.codes.ok:
             raise Exception('Ticket query failed with error ' + str(r.status_code))
@@ -32,6 +36,16 @@ class ServiceNowAPI:
 
 if __name__ == '__main__':
     snow = ServiceNowAPI()
+
+    with open('config.json', 'r') as cf:
+        config = json.loads(cf.read())
+        base_uri = config['instance_uri']
+        username = config['username']
+        password = config['password']
+
+        snow.set_base_uri(base_uri)
+        snow.set_auth(username, password)
+
     result = snow.get_table_schema('cmdb_ci_hardware')
 
     
